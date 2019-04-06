74a69cc33abc3e3a1c4202106fdef4c0bee421fa
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-04-05 20:15:03 -0400

    add auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,31 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+  skip_before_action :authorized, only: %i[create]
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+    def show
+        jwt = request.headers['Authorization']
+        without = jwt.split('Bearer ')
+        id = JWT.decode(without[1], "my_s3cr3t")[0]["user_id"]
+        @user = User.find(id)
+        render json: {user: UserSerializer.new(@user)}
       end
 
-    private
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :user_name, :password, :email)
-    end
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_name, :password, :email)  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create]
-      post '/login', to: 'users#create'
-      get '/profile', to: 'users#profile'
+      post '/signup', to: 'users#create'
+      post '/login', to: 'auth#create'
+      get '/current_user', to: 'users#show'
+      # get '/profile', to: 'users#profile'
     end
   end
 
-  #  get '/', to: 'ebay#create'
-   post '/users', to: 'users#create'
-   get '/users', to: 'users#index'
+  # get '/current_user',  to: 'application#show'
+  
 end
